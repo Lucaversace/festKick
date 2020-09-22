05b54082ee4fabb4d0f0e7e5a40095e8e333222d
Kévin Vallin <kevin.vallin@ynov.com>
2020-09-22 20:22:26 +0200

    marqueur ajouté unique à la Hall Tonie ganier

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,5 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
-
-
-
-
-
 
 @Component({
   selector: 'app-map',
@@ -16,20 +10,58 @@ export class MapComponent implements AfterViewInit {
 
   @ViewChild('map') private map;
 
+  smallicon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
+    iconSize:    [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize:  [41, 41]
+  }); 
+  
   constructor() { }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit(): void { 
+    this.createMap();
+  }
+  createMap() {
+    const Halltonie = {
+      lat: 45.731904,
+      lng: 4.825906,
+    };
 
-    this.map = L.map('map',{
-      center: [45.7, 4.8],
-      zoom: 5
+    const zoomLevel = 12;
+
+    this.map = L.map('map', {
+      center: [Halltonie.lat, Halltonie.lng],
+      zoom: zoomLevel
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
-      tiles.addTo(this.map);
-    } 
+    const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 12,
+      maxZoom: 17,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    mainLayer.addTo(this.map);
+    const Halldescription = `La Halle Tony-Garnier est une salle de concert située dans le quartier de Gerland à Lyon.
+    20 Place Docteurs Charles et Christophe Mérieux, 69007 Lyon`;
+    const popupOptions = {
+      coords: Halltonie,
+      text: Halldescription,
+      open: false
+    };
+    this.addMarker(popupOptions);
   }
+
+  addMarker({coords, text, open}) {
+    const marker = L.marker([coords.lat, coords.lng], { icon: this.smallicon });
+    if (open) {
+      marker.addTo(this.map).bindPopup(text).openPopup();
+    } else {
+      marker.addTo(this.map).bindPopup(text);
+    }
+  }
+}
 
